06c7da7c301dfc1059b9187e931fb2fec63ae751
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2022-10-18 10:56:46 -0500

    strings

--- js/whiteboards.js
+++ js/whiteboards.js
@@ -1,3 +1,50 @@
+// In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+
+// The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+
+// Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+
+// You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+// The string has a length greater or equal to one and contains only letters from a to z.
+
+// Examples:
+// s="aaabbbbhaijjjm"
+// printer_error(s) => "0/14"
+
+// s="aaaxbbbbyyhwawiwjjjwwm"
+// printer_error(s) => "8/22"
+
+// function printerError(s){
+//     // takes in a string, outputs a fraction as a string "numerator/denominator"
+//     // let errors = 0
+//     // for (let c of s){
+//     //     if ('nopqrstuvwxyz'.includes(c)){
+//     //         errors += 1
+//     //     }
+//     // }
+//     let errors = s.split('').filter(c => c > 'm')
+//     return `${errors.length}/${s.length}`
+//     // return slist
+// }
+
+// console.log(printerError('aaabbbcccno'))
+
+
+// function openOrSenior(data){
+//     //   logic: look at each pair, see if age > 54 and handicap > 7
+//     //   let playerType = []
+//     //   for (let player of data){
+//     //     player[0] >= 55 && player[1] > 7 ? playerType.push("Senior") : playerType.push("Open")
+//     //   }
+//     //   return playerType
+//     // }
+
+//     // option 2:
+//     return data.map(([age, handicap]) => (age >= 55 && handicap > 7) ? "Senior" : "Open")
+// }
+
+// console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]),['Open', 'Senior', 'Open', 'Senior'])
 
 
 // function findNextSquare(sq) {
