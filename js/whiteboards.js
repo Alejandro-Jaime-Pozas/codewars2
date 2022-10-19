66b147ec40320fbfdefd729e9683540fe7cc113c
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2022-10-19 10:04:41 -0500

    a

--- js/whiteboards.js
+++ js/whiteboards.js
@@ -1,3 +1,20 @@
+// function reverseWords(str) {
+//     // Go for it
+//     return str[0]
+//   }
+
+// console.log(reverseWords('The quick brown fox jumps over the lazy dog.'));
+
+// function number(busStops){
+//     // need to extract arr within arr to access the numbers...
+//     // for each pair, subtract right side from left, add to global count variable
+//     let count = 0
+//     busStops.map(([hopOn, hopOff]) => count += hopOn - hopOff)
+//     return count
+// }
+
+// console.log(number([[10,0],[3,5],[5,8]])); // 5
+
 // In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
 
 // The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
