21cfe194e8fa3438de08b7ab07173cc1bc6fbba5
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2022-11-01 12:31:25 -0500

    s

--- wb.py
+++ wb.py
@@ -1,5 +1,34 @@
+# Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+# HH = hours, padded to 2 digits, range: 00 - 99
+# MM = minutes, padded to 2 digits, range: 00 - 59
+# SS = seconds, padded to 2 digits, range: 00 - 59
+# The maximum time never exceeds 359999 (99:59:59)
+
+# You can find some examples in the test fixtures.
+
+def make_readable(seconds):
+    # input -> int
+    # output -> str w hh:mm:ss as numbers
+    # need to figure out how each s, m, h will display its number accordingly...
+    # can take a number and retain its remainder converted to s, then m, then h...
+    # need to first check its remainder as divided by 60, then its remainder by 3600? that would give you the resting minutes in seconds, so would need to convert those seconds to minute notation probably? same with hours?
+    # if divide 21 / 10, you get 1 as remainder, 25 / 10 you get 5. its all in its base notation
+
+    # logically, I would split the number into chunks
+    # last chunk div by 60
+    secs = '0' + str(seconds % 60) if seconds % 60 < 10 else str(seconds % 60) # gets seconds
+    mins = '0' + str((seconds % (60*60)) // 60 ) if ((seconds % (60*60)) // 60 ) < 10 else str((seconds % (60*60)) // 60 ) # need minutes
+    hrs = '0' + str((seconds // (60*60))) if ((seconds // (60*60))) < 10 else str((seconds // (60*60))) # gets hours
+    return f'{hrs}:{mins}:{secs}'
+    
 
 
+print(make_readable(0))
+print(make_readable(5))
+print(make_readable(60))
+print(make_readable(86399))
+print(make_readable(359999))
 
 # def narcissistic(value):
 #     exp = len(str(value))
