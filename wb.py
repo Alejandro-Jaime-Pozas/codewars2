6cbf48cff52d3348575fa8748d84f7257844661e
Alex Jaime <ajaimepozas@gmail.com>
2023-05-08 10:03:31 -0600

    primes

--- wb.py
+++ wb.py
@@ -1,4 +1,12 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for n in range(2, num//2+1):
+        if num % n == 0:
+            return False 
+    return True
 
+print(is_prime(31))
 
 
 # def tribonacci(signature, n):
