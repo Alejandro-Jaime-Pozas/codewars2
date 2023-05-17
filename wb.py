474432347acb4d2ae9a8389ee721abf2cc6afa2a
Alex Jaime <ajaimepozas@gmail.com>
2023-05-17 09:59:38 -0600

    romans

--- wb.py
+++ wb.py
@@ -1,13 +1,32 @@
-def find_nb(m):
-    # recursive fn
-    total = 0
-    n = 1
-    while total < m:
-        total += n**3
-        n += 1
-    return n-1 if total == m else -1
+# def solution(n):
+#     # between 1 and 3999
+#     # Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero. 
+#     # In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
 
-print(find_nb(135440716410000)) # 3
+#     # always checking the highest divisible value first. then go down a level (3999 checks if / 1000)
+#     romans = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']
+#     values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+#     final = ''
+#     for i, v in enumerate(values):
+#         calc = n // v
+#         if calc > 0:
+#             final += calc*romans[i]
+#             n -= calc*v
+#     return final 
+
+# print(solution(1990))
+
+
+# def find_nb(m):
+#     # recursive fn
+#     total = 0
+#     n = 1
+#     while total < m:
+#         total += n**3
+#         n += 1
+#     return n-1 if total == m else -1
+
+# print(find_nb(135440716410000)) # 3
 
 
 # def count_smileys(arr):
