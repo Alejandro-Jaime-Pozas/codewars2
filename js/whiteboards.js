529b0509b125561d4cd21248c0258839668ac01e
Alex Jaime <ajaimepozas@gmail.com>
2023-06-12 10:55:11 -0600

    even

--- js/whiteboards.js
+++ js/whiteboards.js
@@ -1,6 +1,13 @@
 
 
 
+// function evenNumbers(array, number) {
+//     return array.filter(a => a % 2 == 0).slice(-number)
+//   }
+
+// console.log(evenNumbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2));
+
+
 // function getCount(str) {
 //     let counter = 0
 //     const vowels = 'aeiou'
