a84078fcafd5fe69165f137e16f885625caff767
Alex Jaime <ajaimepozas@gmail.com>
2023-08-15 09:43:59 -0600

    s

--- js/whiteboards.js
+++ js/whiteboards.js
@@ -1,4 +1,8 @@
+var greet = function(name) {
+  return 'Hello ' + name.slice(0,1).toUpperCase() + name.slice(1).toLowerCase() + '!'
+};
 
+console.log(greet('JACK'));
 
 
 // function partsSums(lst) {
