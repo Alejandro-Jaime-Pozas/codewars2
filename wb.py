0aed8234ffe3a5294bf44d72fd8b08507e883900
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2024-05-29 12:53:19 -0600

    sort

--- wb.py
+++ wb.py
@@ -1,4 +1,15 @@
+def merge(nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+    """
+    Do not return anything, modify nums1 in-place instead.
+    """
+    for i in range(n):
+        nums1[m+i] = nums2[i]
 
+    nums1.sort()
+    print(nums1)
+
+
+print(merge([2,2,3,0,0,0], 3, [1,5,6], 3))
 
 
 # # Remove duplicates from sorted list
